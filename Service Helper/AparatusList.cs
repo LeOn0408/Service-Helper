2cb01e66f6109a34a9eb84b03ad4853151776608
aparshukov <i@aparshukov.ru>
2021-03-31 21:16:58 +0300

    0.01a

---
+++ Service Helper/AparatusList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service_Helper
+{
+    class AparatusList
+    {
+        private List<string> aparatusList = new List<string>{
+            "Разное",
+            "Смартфоны",
+            "Телевизоры"
+        };
+        public List<string> GetList() => aparatusList;
+    }
+}
--- Service Helper/MainWindow.xaml.cs
+++ Service Helper/MainWindow.xaml.cs
@@ -20,9 +20,47 @@ namespace Service_Helper
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string scuffs, stringToOneS;
+        AparatusList aparatusList = new();
         public MainWindow()
         {
             InitializeComponent();
+            CopyPaste();
+
+            
+
+            copyToClipboard.Click += CopyToClipboard_Click;
+
+            apparatus.ItemsSource = aparatusList.GetList();
+            apparatus.SelectedIndex = 0;
+            
+            
+            EngineerWarning.AppendText("Проверьте состояние дисплея и корпуса!!!");
+            
+        }
+
+        private void CopyToClipboard_Click(object sender, RoutedEventArgs e)
+        {
+            string richText = new TextRange(copyPaste.Document.ContentStart, copyPaste.Document.ContentEnd).Text;
+            Clipboard.SetText(richText);
+        }
+        
+        private void CopyPaste()
+        {
+            stringToOneS = "Б/У, " + scuffs;
+            copyPaste.Document.Blocks.Clear();
+            copyPaste.AppendText(stringToOneS);
+        }
+        
+        private void scuffs_Checked(object sender, RoutedEventArgs e)
+        {
+            scuffs = "потертости ";
+            CopyPaste();
+        }
+        private void scuffs_Unchecked(object sender, RoutedEventArgs e)
+        {
+            scuffs = "";
+            CopyPaste();
         }
     }
 }
