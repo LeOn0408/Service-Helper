cb92491d426b5cbfa34c666bf9f66d9392c15283
aparshukov <i@aparshukov.ru>
2021-04-13 23:53:17 +0300

    Подгрузка строки из Словаря, а не из Списка

--- Service Helper/MainWindow.xaml.cs
+++ Service Helper/MainWindow.xaml.cs
@@ -11,13 +11,16 @@ namespace Service_Helper
     public partial class MainWindow : Window
     {
         AparatusList aparatusList = new();
-        CheckList checkList = new();
+        DisplayingLists displayingLists = new();
         Appearance appearance = new();
-        
+        XmlLoad xmlLoad = new();
         public MainWindow()
         {
             InitializeComponent();
+            xmlLoad.xmlLoad();
             createCheckBox();
+            
+
             copyToClipboard.Click += CopyToClipboard_Click;
             apparatus.ItemsSource = aparatusList.GetList;
             apparatus.SelectedIndex = 0;
@@ -31,60 +34,86 @@ namespace Service_Helper
             Clipboard.SetText(richText);
         }
         
-        
-        
         private void createCheckBox()
         {
-            List<string> list = checkList.GetList;
-            int countCheck = list.Count;
-            for (int i = 0; i< countCheck;i++) 
+            List<CheckList> list = displayingLists.GetCheckLists;
+            foreach (CheckList element in list)
             {
-                string content = list[i];
-                CheckBox checkBox = new();
-                checkBox = checkList.GetCheck(content, i);
-                checkBox.Checked += checkBox_Checked;
-                checkBox.Unchecked += checkBox_Unchecked;
-                checkBoxList.Items.Add(checkBox);
+                if (element.Category == "Minor")
+                {
+                    CheckBox checkBox = new CheckBox
+                    {
+                        Name = element.Name,
+                        Content = element.Content,
+                        Tag = element.Category
+                    };
+                    checkBox.Checked += checkBox_Checked;
+                    checkBox.Unchecked += checkBox_Unchecked;
+                    checkBoxList.Items.Add(checkBox);
+                }
+                else if (element.Category == "Kit")
+                {
+                    CheckBox checkBox = new CheckBox
+                    {
+                        Name = element.Name,
+                        Content = element.Content,
+                        Tag = element.Category
+                    };
+                    checkBox.Checked += checkBox_Checked;
+                    checkBox.Unchecked += checkBox_Unchecked;
+                    kitBoxList.Items.Add(checkBox);
+                }
             }
 
         }
         private void checkBox_Checked(object sender, RoutedEventArgs e)
         {
             CheckBox chBox = (CheckBox)sender;
-            int index = Convert.ToInt32(chBox.Tag);
-            appearance.setChBox(index, chBox.Content.ToString());
+            string index = chBox.Name.ToString();
+            string content = chBox.Content.ToString();//формирует нижнюю строку. Будет дальше расширятся и использоваться
+            //string category = chBox.Tag.ToString();
+            string category = chBox.Tag.ToString();
+            appearance.SetList(index, content, category);
             FillTextBox();
         }
         private void checkBox_Unchecked(object sender, RoutedEventArgs e)
         {
             CheckBox chBox = (CheckBox)sender;
-            int index = Convert.ToInt32(chBox.Tag);
-            appearance.delChBox(index);
+            string index = chBox.Name.ToString();
+            appearance.DelList(index);
             FillTextBox();
         }
 
         private void FillTextBox()
         {
-            Dictionary<int, string> appearanceText = appearance.GetList;
-            string richText="";
+            Dictionary<string, CheckList> list = appearance.GetLists;
+            string minor = "";
+            string kit= "";
             copyPasteString.Document.Blocks.Clear();
-            foreach (string element in appearanceText.Values)
+            foreach (CheckList element in list.Values)
             {
-                richText = richText + ", " + element.ToLower();
+                if (element.Category == "Minor")
+                {
+                    minor = minor + ", " + element.Content.ToLower();
+                }
+                if (element.Category == "Kit")
+                {
+                    kit = kit + ", " + element.Content.ToLower();
+                }
             }
-            // Сломатый трехногий костыль, пока сам код в проработке
-            richText = richText + Damage() + ". ";
-            //
+            copyPasteString.AppendText("Б/У" + minor + Damage()+ kit);
             
-            copyPasteString.AppendText("Б/У" + richText);
         }
-        private string Damage()
+        
+        private string Damage()//костыль. Создать обьект
         {
             string str="";
             if (@case.IsChecked == true)
                 str = str + ", погнут корпус";
             if(screen.IsChecked == true)
                 str = str + ", разбит дисплей";
+            if (screen.IsChecked == false && screenNotDamaged.IsChecked == false)
+                str = str + ", состояние дисплея неизвестно";
             if(connector.IsChecked == true)
                 str = str + ", сломан разъем";
             return str;
@@ -95,6 +124,7 @@ namespace Service_Helper
         {
             appearance.delAll();
             checkBoxList.Items.Clear();
+            kitBoxList.Items.Clear();
             createCheckBox();
             FillTextBox();
         }
