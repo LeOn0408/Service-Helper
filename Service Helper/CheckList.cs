0bfdd939d7eeb208bc0f4352d8544dc2cb2c37ea
aparshukov <i@aparshukov.ru>
2021-04-13 23:50:29 +0300

    Удаление лишних строк

--- Service Helper/CheckList.cs
+++ Service Helper/CheckList.cs
@@ -1,33 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Controls;
-
-namespace Service_Helper
+﻿namespace Service_Helper
 {
     class CheckList
     {
-        public CheckList()
-        {
-
-        }
-        private List<string> checkList = new List<string>{
-            "Потертости",
-            "Царапины",
-            "Вмятины",
-            "Следы эксплуатации"
-        };
-        public List<string> GetList => checkList;
-        
-        public CheckBox GetCheck(string content,int name)
-        {
-            CheckBox checkBox2 = new CheckBox
-            {
-                Tag = name,
-                HorizontalContentAlignment = HorizontalAlignment.Left,
-                VerticalContentAlignment = VerticalAlignment.Top,
-                Content = content,
-            };
-            return checkBox2;
-        }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Content { get; set; }
+        public string GroupName { get; set; }
     }
 }
