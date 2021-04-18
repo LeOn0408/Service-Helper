ac6e3a6eadb2296f9b518139b2da36108c70a311
Александр Паршуков <20384838+LeOn0408@users.noreply.github.com>
2021-04-18 09:51:57 +0300

    Небольшие исправления

--- Service Helper/DisplayingLists.cs
+++ Service Helper/DisplayingLists.cs
@@ -23,11 +23,12 @@ namespace Service_Helper
                 //Третий столбец с комплектацией
                 new CheckList  { Name = "testkit1", Category = "Kit", Content ="Упаковка"},
                 new CheckList  { Name = "testkit2", Category = "Kit", Content ="Аппарат"},
-                new CheckList  { Name = "testkit3", Category = "Kit", Content ="Зврядное устройство"},
+                new CheckList  { Name = "testkit3", Category = "Kit", Content ="Зарядное устройство"},
                 new CheckList  { Name = "testkit4", Category = "Kit", Content ="USB-кабель"},
                 new CheckList  { Name = "testkit5", Category = "Kit", Content ="Документы"},
                 new CheckList  { Name = "testkit6", Category = "Kit", Content ="Защитное стекло"},
                 new CheckList  { Name = "testkit7", Category = "Kit", Content ="Ключ слота сим"},
+                new CheckList  { Name = "testkit7", Category = "Kit", Content ="Наушники"},
             };
         }
         public List<CheckList> GetCheckLists => checkLists;
